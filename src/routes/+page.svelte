f084f57508ca0b0a8fe933ee82c988f1e04374fb
Srikant Gudi <srikantgdev@gmail.com>
2023-08-18 12:43:04 +0530

    latest commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,104 @@
+<script>
+  let dd = 1;
+  let mo = 1;
+  let yr;
+  const dt = new Date();
+
+  let age = {
+    years: 0,
+    months: 0,
+    days: 0
+  }
+  let maxyear = yr = (new Date()).getFullYear();
+  $: {
+    age.years = (dt.getFullYear() - yr) || 0; 
+    age.months = (dt.getMonth() + 1) - mo || 0;
+    if (age.months < 0) {
+      age.years--;
+      age.months += 12;
+    }
+    age.days = (dt.getDate() - dd) || 0; 
+  }
+</script>
+<style>
+  .attribution { font-size: 11px; text-align: center; }
+  .attribution a { color: hsl(228, 45%, 44%); }
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    align-items: center;
+    font-size: 1.2rem;
+    margin: auto 4rem 1rem;
+  }
+  .app-title {
+    font-size: 1.3vw;
+    margin-bottom: 1rem;
+    text-align: center;
+  }
+  input {
+    font-size: 1.2rem;
+    border: none;
+    box-shadow: 0 0 4px #999;
+    padding: 0.5rem;
+  }
+  main {
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    padding: 2rem;
+    border-radius: 1rem 1rem 8rem 1rem;
+    box-shadow: 0 0 4px #999;
+  }
+  .age-box {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding: 1rem;
+    margin: 1rem auto 1rem;
+    width: fit-content;
+  }
+  .age {
+    font-family: helvetica;
+    font-size: 2rem;
+    font-style: italic;
+    font-weight: 600;
+    color: #333;
+  }
+  .arrow {
+    background: url(../assets/images/icon-arrow.svg) no-repeat 0 50%;
+  }
+  @media screen and (max-width: 480px) {
+    .app-title {
+      font-size: 5vw;
+    }
+  }
+</style>
+
+<main>
+  <div class="app-title">Frontend Mentor | Age calculator app</div>
+
+  <div class="grid">
+    <div>Day</div>
+    <div>Month</div>
+    <div>Year</div>
+  </div>
+  <div class="grid">
+    <div><input type="number" bind:value={dd} min={1} max={31}></div>
+    <div><input type="number" bind:value={mo} min={1} max={12}></div>
+    <div><input type="number" bind:value={yr} min={1900} max={maxyear}></div>
+  </div>
+  <div class="arrow">
+    <hr>
+  </div>
+  <div class="age-box">
+    <div class="age">{age.years} years</div>
+    <div class="age">{age.months} months</div>
+    <div class="age">{age.days} days</div>
+  </div>
+
+  <div class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+    Coded by <a href="https://github.com/srikantgudi/age-calculator" target="_app">Srikant Gudi</a>.
+  </div>
+</main>
